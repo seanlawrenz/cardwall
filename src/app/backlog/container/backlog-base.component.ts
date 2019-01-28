1331d205386db333ccdc56117c160513e52d5799
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-01-28 10:23:05 -0500

    backlog base wired to ngrx

--- src/app/backlog/container/backlog-base.component.ts
+++ src/app/backlog/container/backlog-base.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import * as fromBacklog from '../state';
+import * as backlogActions from '../state/backlog.actions';
+
+import { PlanIdentifier } from '@app/models';
 
 @Component({
   selector: 'app-backlog-base',
@@ -7,7 +14,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BacklogBaseComponent implements OnInit {
   projects = '';
-  constructor() {}
+  planIdentifiers$: Observable<PlanIdentifier[]>;
+  errorMessage$: Observable<string>;
 
-  ngOnInit() {}
+  constructor(private store: Store<fromBacklog.State>) {}
+
+  ngOnInit() {
+    this.store.dispatch(new backlogActions.GetAvailableBoards());
+
+    this.planIdentifiers$ = this.store.pipe(select(fromBacklog.getPlans));
+    this.errorMessage$ = this.store.pipe(select(fromBacklog.getPlansError));
+  }
 }
