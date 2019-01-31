8f459501cebed8f8f82779b733730b6e68a23b3c
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-01-31 16:06:10 -0500

    tests passing

--- src/app/backlog/components/backlog-container/backlog-nav/backlog-move-toolbar/backlog-move-toolbar.component.spec.ts
+++ src/app/backlog/components/backlog-container/backlog-nav/backlog-move-toolbar/backlog-move-toolbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { BacklogMoveToolbarComponent } from './backlog-move-toolbar.component';
+import { ButtonComponent } from '@app/shared/components/button/button.component';
 
 describe('BacklogMoveToolbarComponent', () => {
   let component: BacklogMoveToolbarComponent;
@@ -8,9 +10,9 @@ describe('BacklogMoveToolbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BacklogMoveToolbarComponent ]
-    })
-    .compileComponents();
+      declarations: [BacklogMoveToolbarComponent, ButtonComponent],
+      imports: [TooltipModule.forRoot()],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
