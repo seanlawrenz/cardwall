abe15ac08e15eda468e9cf5dac38b0a09f5c3a51
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-02-05 15:35:57 -0500

    card counts estimated hours story points summed

--- src/app/backlog/components/backlog-container/boards-view/backlog-board-header/backlog-board-header.component.spec.ts
+++ src/app/backlog/components/backlog-container/boards-view/backlog-board-header/backlog-board-header.component.spec.ts
@@ -2,7 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 
 import { BacklogBoardHeaderComponent } from './backlog-board-header.component';
-import { mockBoard } from '@app/test/data';
+import { mockBoard, mockCard, mockList } from '@app/test/data';
+import { Card, List, Plan } from '@app/models';
 
 describe('BacklogBoardHeaderComponent', () => {
   let component: BacklogBoardHeaderComponent;
@@ -18,11 +19,45 @@ describe('BacklogBoardHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BacklogBoardHeaderComponent);
     component = fixture.componentInstance;
-    component.plan = mockBoard;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('updateSummaryData', () => {
+    it('should update the estimateHours and storyPoints', async(() => {
+      const activeMockList: List = Object.assign({}, mockList, { active: true });
+      const mockCardWithHours: Card = Object.assign({}, mockCard, { planId: 123, estimatedHours: 3, storyPoints: 1 });
+      const extraMockCardWithHours: Card = Object.assign({}, mockCard, { planId: 123, estimatedHours: 5, storyPoints: 3 });
+      activeMockList.cards.push(mockCardWithHours, extraMockCardWithHours);
+      const archiveList: List = Object.assign({}, mockList, { id: 0, cards: [mockCard, mockCardWithHours] });
+      const inactiveList: List = Object.assign({}, mockList, { active: false, cards: [mockCard, extraMockCardWithHours] });
+      const mockPlanWithHours: Plan = Object.assign({}, mockBoard, { id: 123, lists: [activeMockList, archiveList, inactiveList] });
+      component.plan = mockPlanWithHours;
+
+      fixture.detectChanges();
+
+      component.updateSummaryData();
+
+      expect(component.cardCount).toEqual(2);
+      expect(component.estimatedHours).toEqual(8);
+      expect(component.storyPoints).toEqual(4);
+    }));
+
+    it('should handle an empty plan', () => {
+      component.plan = undefined;
+      expect(() => {
+        component.updateSummaryData();
+      }).not.toThrowError();
+    });
+
+    it('should handle a plan with no lists', () => {
+      component.plan = mockBoard;
+      expect(() => {
+        component.updateSummaryData();
+        expect(component.cardCount).toEqual(0);
+      }).not.toThrowError();
+    });
   });
 });
--- src/app/backlog/components/backlog-container/boards-view/backlog-board-header/backlog-board-header.component.ts
+++ src/app/backlog/components/backlog-container/boards-view/backlog-board-header/backlog-board-header.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { Plan } from '@app/models';
-import { isNullOrUndefined } from 'util';
+
+import { Card, List } from '@app/models';
+
+import { flatMap, flatMapDeep, sum } from 'lodash';
 
 @Component({
   selector: 'td-backlog-board-header',
   templateUrl: './backlog-board-header.component.html',
   styleUrls: ['./backlog-board-header.component.scss'],
 })
-export class BacklogBoardHeaderComponent implements OnInit {
+export class BacklogBoardHeaderComponent implements OnInit, OnChanges {
   @Input() plan: Plan;
+  cardCount = 0;
+  estimatedHours = 0;
+  storyPoints = 0;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateSummaryData();
+  }
+
+  ngOnChanges() {
+    this.updateSummaryData();
+  }
+
+  updateSummaryData() {
+    if (!this.plan || this.plan.lists.length === 0) {
+      return;
+    }
+
+    const sumArray = (arrayOfNumbers: number[]) => sum(arrayOfNumbers);
+    const flatPlanCards = (lists: List[]) => flatMap(lists.map(list => list.cards));
+    const flatPlanCardsLength = (lists: List[]) => flatMapDeep(lists.map(list => list.cards.length));
+    const flatMapHours = (cards: Card[]) => flatMapDeep(cards.map(card => card.estimatedHours));
+    const flatMapStoryPoints = (cards: Card[]) => flatMapDeep(cards.map(card => card.storyPoints));
+
+    const activeLists = this.plan.lists.filter(list => list.active && list.id > 0);
+    this.cardCount = sumArray(flatPlanCardsLength(activeLists));
+    this.estimatedHours = sumArray(flatMapHours(flatPlanCards(activeLists)));
+    this.storyPoints = sumArray(flatMapStoryPoints(flatPlanCards(activeLists)));
+  }
 }
