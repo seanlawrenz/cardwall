91665823a2699ac902cbf5247dbf0ac2d3ce6a44
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-03-08 11:00:29 -0500

    simple move funtionality built

--- src/app/backlog/container/backlog-move-toolbar/backlog-move-toolbar.component.spec.ts
+++ src/app/backlog/container/backlog-move-toolbar/backlog-move-toolbar.component.spec.ts
@@ -4,25 +4,34 @@ import { TdTooltipDirective } from '@app/shared/directives/tooltip-directive';
 import { BacklogMoveToolbarComponent } from './backlog-move-toolbar.component';
 import { ButtonComponent } from '@app/shared/components/button/button.component';
 import { Store } from '@ngrx/store';
+import { hot } from 'jasmine-marbles';
 
 describe('BacklogMoveToolbarComponent', () => {
   let component: BacklogMoveToolbarComponent;
   let fixture: ComponentFixture<BacklogMoveToolbarComponent>;
+  let store;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [BacklogMoveToolbarComponent, ButtonComponent, TdTooltipDirective],
-      providers: [{ provide: Store, useValue: { dispatch: jest.fn() } }],
+      providers: [{ provide: Store, useValue: { dispatch: jest.fn(), pipe: jest.fn(), select: jest.fn() } }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BacklogMoveToolbarComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store = TestBed.get(Store);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should subscribe to the store for select card', () => {
+  //   store.pipe = hot('-a', { a: undefined });
+  //   const spy = jest.spyOn(store, 'select');
+  //   fixture.detectChanges();
+  //   expect(spy).toHaveBeenCalled();
+  // });
 });
--- src/app/backlog/container/backlog-move-toolbar/backlog-move-toolbar.component.ts
+++ src/app/backlog/container/backlog-move-toolbar/backlog-move-toolbar.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 
 import { fromRoot } from '@app/store';
+import * as fromBacklog from '@app/backlog/state';
 import * as fromUI from '@app/store/actions/ui.actions';
+import { Card } from '@app/models';
 
 @Component({
   selector: 'td-backlog-move-toolbar',
   templateUrl: './backlog-move-toolbar.component.html',
   styleUrls: ['./backlog-move-toolbar.component.scss'],
 })
-export class BacklogMoveToolbarComponent implements OnInit {
-  constructor(private store: Store<fromRoot.State>) {}
+export class BacklogMoveToolbarComponent implements OnInit, OnDestroy {
+  selectedCard: Card;
+  selectSub: Subscription;
+  listSub: Subscription;
+  planSub: Subscription;
 
-  ngOnInit() {}
+  canMoveUp = false;
+  canMoveDown = false;
+
+  constructor(private store: Store<fromRoot.State>, private appStore: Store<fromRoot.State>) {}
+
+  ngOnInit() {
+    this.selectSub = this.appStore.pipe(select(fromRoot.getSelectedCard)).subscribe(card => {
+      this.onSelectedCard(card);
+    });
+  }
+
+  ngOnDestroy() {
+    this.selectSub.unsubscribe();
+    this.listSub.unsubscribe();
+  }
 
   expandAllLists() {
     this.store.dispatch(new fromUI.ExpandAllLists());
@@ -20,5 +40,20 @@ export class BacklogMoveToolbarComponent implements OnInit {
 
   collapseAllLists() {
     this.store.dispatch(new fromUI.CompressAllLists());
+  }
+
+  private onSelectedCard(card: Card) {
+    this.selectedCard = card;
+    if (this.selectedCard !== undefined) {
+      this.listSub = this.store
+        .pipe(select(fromBacklog.getListById(this.selectedCard.planId, this.selectedCard.listId)))
+        .subscribe(list => {
+          const cardIndex: number = list.cards.findIndex(c => c.id === card.id);
+
+          this.canMoveDown = cardIndex >= 0 && cardIndex < list.cards.length - 1;
+
+          this.canMoveUp = cardIndex > 0;
+        });
+    }
   }
 }
