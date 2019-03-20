6f73a6016d007dd042091ad835a94022837884fd
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-03-20 14:53:53 -0400

    edit form permissons

--- src/app/card-details/components/edit-card-form/edit-card-form.component.spec.ts
+++ src/app/card-details/components/edit-card-form/edit-card-form.component.spec.ts
@@ -1,25 +1,91 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditCardFormComponent } from './edit-card-form.component';
+import { ConfigService } from '@app/app-services';
+import { mockConfigService } from '@app/test/mocks';
+import { mockCard, mockResource } from '@app/test/data';
 
 describe('EditCardFormComponent', () => {
   let component: EditCardFormComponent;
   let fixture: ComponentFixture<EditCardFormComponent>;
+  let configService: ConfigService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditCardFormComponent ]
-    })
-    .compileComponents();
+      declarations: [EditCardFormComponent],
+      providers: [{ provide: ConfigService, useValue: mockConfigService }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditCardFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('setPermissions', () => {
+    beforeEach(() => {
+      configService = TestBed.get(ConfigService);
+    });
+
+    it('should not allow permission to someone who cannot update tasks or edit', () => {
+      configService.config.CanUpdateTasks = false;
+      configService.config.CanUpdateMyTasksOnly = false;
+      configService.config.CanEditTasks = false;
+      component.card = mockCard;
+
+      fixture.detectChanges();
+
+      expect(component.canEditOrUpdate).toBeFalsy();
+    });
+
+    it('should allow permission to someone who can update tasks', () => {
+      configService.config.CanUpdateTasks = true;
+      component.card = mockCard;
+
+      fixture.detectChanges();
+
+      expect(component.canEditOrUpdate).toBeTruthy();
+    });
+
+    it('should allow permission to someone who can update their own tasks and assigned to it', () => {
+      configService.config.CanUpdateTasks = false;
+      configService.config.CanUpdateMyTasksOnly = true;
+      const mockCardWithThisOwner = { ...mockCard, owners: [mockResource] };
+      configService.config.UID = mockResource.uid;
+      component.card = mockCardWithThisOwner;
+
+      fixture.detectChanges();
+
+      expect(component.isAssigned).toBeTruthy();
+      expect(component.canEditOrUpdate).toBeTruthy();
+    });
+
+    it('should not allow permission to someone who can update their own tasks but not assigned to it', () => {
+      configService.config.CanUpdateTasks = false;
+      configService.config.CanUpdateMyTasksOnly = true;
+      const mockCardWithNoOwners = { ...mockCard, owners: [] };
+      configService.config.UID = mockResource.uid;
+      component.card = mockCardWithNoOwners;
+
+      fixture.detectChanges();
+
+      expect(component.isAssigned).toBeFalsy();
+      expect(component.canEditOrUpdate).toBeFalsy();
+    });
+
+    it('should allow permissions to someone who can edit tasks', () => {
+      configService.config.CanUpdateTasks = false;
+      configService.config.CanUpdateMyTasksOnly = false;
+      configService.config.CanEditTasks = true;
+      component.card = mockCard;
+
+      fixture.detectChanges();
+
+      expect(component.canEditOrUpdate).toBeTruthy();
+    });
   });
 });
--- src/app/card-details/components/edit-card-form/edit-card-form.component.ts
+++ src/app/card-details/components/edit-card-form/edit-card-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ConfigService } from '@app/app-services';
+import { Card } from '@app/models';
 
 @Component({
   selector: 'td-edit-card-form',
   templateUrl: './edit-card-form.component.html',
-  styleUrls: ['./edit-card-form.component.scss']
+  styleUrls: ['./edit-card-form.component.scss'],
 })
 export class EditCardFormComponent implements OnInit {
+  @Input() card: Card;
+  // Permissions
+  isAssigned: boolean;
+  canUpdate: boolean;
+  canEdit: boolean;
+  canEditOrUpdate: boolean;
 
-  constructor() { }
+  constructor(private config: ConfigService) {}
 
   ngOnInit() {
+    this.setPermissions();
   }
 
+  private setPermissions() {
+    // Determine whether or not the resource is assigned to the task
+    this.isAssigned =
+      this.card.owners && this.card.owners.findIndex(o => o.uid.toLowerCase() === this.config.config.UID.toLowerCase()) >= 0;
+    // If the user is assigned to the card and has can update my tasks only, they can update, otherwise check the standard permission
+    if (this.config.config.CanUpdateTasks) {
+      this.canEditOrUpdate = true;
+    } else if (this.config.config.CanUpdateMyTasksOnly) {
+      this.canEditOrUpdate = this.isAssigned;
+    } else {
+      this.canEditOrUpdate = this.config.config.CanEditTasks;
+    }
+
+    this.canEdit = this.config.config.CanEditTasks;
+  }
 }
