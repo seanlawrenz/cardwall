1d83a400c9fdd297733bdad0b5aead251400bbf5
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-03-25 09:42:51 -0400

    Put notifications on index

--- src/app/app-services/index.ts
+++ src/app/app-services/index.ts
@@ -2,4 +2,5 @@ export * from './app.service';
 export * from './board.service';
 export * from './card.service';
 export * from './config.service';
+export * from './notification.service';
 export * from './signal-r.service';
