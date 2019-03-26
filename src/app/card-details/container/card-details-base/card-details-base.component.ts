8526c78124f5d8a7ba95859c47c6d08f076e094a
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-03-26 15:48:43 -0400

    Added async support for tags

--- src/app/card-details/container/card-details-base/card-details-base.component.ts
+++ src/app/card-details/container/card-details-base/card-details-base.component.ts
@@ -11,6 +11,7 @@ import { Card, CardDetailsPageTypes, Plan, Board } from '@app/models';
   selector: 'td-card-details-base',
   templateUrl: './card-details-base.component.html',
   styleUrls: ['./card-details-base.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardDetailsBaseComponent implements OnInit {
   card$: Observable<Card>;
