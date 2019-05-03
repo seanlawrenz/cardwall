ab7697f5b81d47cd117a28bb14bc904288eb4f87
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-05-03 16:54:55 -0400

    base setup for attachements

--- src/app/card-details/container/attachments-base/attachments-base.component.spec.ts
+++ src/app/card-details/container/attachments-base/attachments-base.component.spec.ts
@@ -1,25 +1,93 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
+import { hot, getTestScheduler } from 'jasmine-marbles';
+
+import * as attachmentActions from '@app/card-details/state/actions/attachment.actions';
 
 import { AttachmentsBaseComponent } from './attachments-base.component';
+import { mockCard, mockAttachment } from '@app/test/data';
+import { ErrorFromSignalR } from '@app/models';
 
 describe('AttachmentsBaseComponent', () => {
   let component: AttachmentsBaseComponent;
   let fixture: ComponentFixture<AttachmentsBaseComponent>;
+  let store;
+  let action;
+  let spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AttachmentsBaseComponent ]
-    })
-    .compileComponents();
+      declarations: [AttachmentsBaseComponent],
+      providers: [{ provide: Store, useValue: { dispatch: jest.fn(), pipe: jest.fn() } }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AttachmentsBaseComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.card = mockCard;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    beforeEach(() => (store = TestBed.get(Store)));
+
+    it('should dispatch to get the attachments', () => {
+      action = new attachmentActions.FetchAttachments(mockCard);
+      spy = jest.spyOn(store, 'dispatch');
+
+      fixture.detectChanges();
+
+      expect(spy).toHaveBeenCalledWith(action);
+    });
+
+    describe('attachments$', () => {
+      it('should be an observable of an array of attachments', done => {
+        store.pipe = jest.fn(() => hot('-a', { a: [mockAttachment] }));
+
+        fixture.detectChanges();
+
+        component.attachments$.subscribe(attachments => {
+          expect(attachments).toEqual([mockAttachment]);
+          done();
+        });
+
+        getTestScheduler().flush();
+      });
+    });
+
+    describe('loading$', () => {
+      it('should be an observable of a boolean', done => {
+        store.pipe = jest.fn(() => hot('-a', { a: true }));
+
+        fixture.detectChanges();
+
+        component.loading$.subscribe(isLoading => {
+          expect(isLoading).toEqual(true);
+          done();
+        });
+
+        getTestScheduler().flush();
+      });
+    });
+
+    describe('errors$', () => {
+      it('should be an observable of an error', done => {
+        const serverError: ErrorFromSignalR = { message: 'Server Error', reason: 'too many secrets' };
+        store.pipe = jest.fn(() => hot('-a', { a: serverError }));
+
+        fixture.detectChanges();
+
+        component.error$.subscribe(error => {
+          expect(error).toEqual(serverError);
+          done();
+        });
+
+        getTestScheduler().flush();
+      });
+    });
   });
 });
--- src/app/card-details/container/attachments-base/attachments-base.component.ts
+++ src/app/card-details/container/attachments-base/attachments-base.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+
+import { fromRoot } from '@app/store';
+import * as selectors from '@app/card-details/state/selectors';
+import * as actions from '@app/card-details/state/actions/attachment.actions';
+
+import { Attachment, ErrorFromSignalR, Card } from '@app/models';
 
 @Component({
   selector: 'td-attachments-base',
   templateUrl: './attachments-base.component.html',
-  styleUrls: ['./attachments-base.component.scss']
+  styleUrls: ['./attachments-base.component.scss'],
 })
 export class AttachmentsBaseComponent implements OnInit {
+  @Input() card: Card;
 
-  constructor() { }
+  attachments$: Observable<Attachment[]>;
+  loading$: Observable<boolean>;
+  error$: Observable<ErrorFromSignalR>;
+
+  constructor(private store: Store<fromRoot.State>) {}
 
   ngOnInit() {
+    this.store.dispatch(new actions.FetchAttachments(this.card));
+    this.attachments$ = this.store.pipe(select(selectors.getAttachments));
+    this.loading$ = this.store.pipe(select(selectors.isAttachmentsLoading));
+    this.error$ = this.store.pipe(select(selectors.getAttachmentError));
   }
-
 }
