f16ed4b445c7a7102692d673c081c16ca9c3e4f4
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-05-22 10:36:23 -0400

    update tests

--- src/app/cardwall/components/cardwall-lists-view/cardwall-list/create-list/create-list.component.spec.ts
+++ src/app/cardwall/components/cardwall-lists-view/cardwall-list/create-list/create-list.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { CreateListComponent } from './create-list.component';
+import { SharedModule } from '@app/shared/shared.module';
+import { mockBoard, mockList } from '@app/test/data';
 
 describe('CreateListComponent', () => {
   let component: CreateListComponent;
@@ -8,14 +12,16 @@ describe('CreateListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateListComponent ]
-    })
-    .compileComponents();
+      declarations: [CreateListComponent],
+      imports: [FormsModule, ModalModule, ReactiveFormsModule, SharedModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateListComponent);
     component = fixture.componentInstance;
+    mockBoard.lists = [mockList];
+    component.board = mockBoard;
     fixture.detectChanges();
   });
 
