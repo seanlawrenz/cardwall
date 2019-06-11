8111f296b238e2ce124304513626d61bf85a2eda
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2019-06-11 14:07:41 -0400

    Task #3724365 search

--- src/app/cardwall/components/cardwall-toolbar/cardwall-resources-view/cardwall-resources-view.component.ts
+++ src/app/cardwall/components/cardwall-toolbar/cardwall-resources-view/cardwall-resources-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Resources } from '@app/models';
 import { FormGroup, FormControl } from '@angular/forms';
 
+import { filter, lowerCase } from 'lodash';
+
 @Component({
   selector: 'td-cardwall-resources-view',
   templateUrl: './cardwall-resources-view.component.html',
@@ -13,13 +15,29 @@ export class CardwallResourcesViewComponent implements OnInit {
   @Output() closeResourcesRequested = new EventEmitter<void>();
 
   formGroup: FormGroup;
+  resourcesFiltered: Resources[];
 
   constructor() {}
 
   ngOnInit() {
+    this.resourcesFiltered = [...this.resources];
     this.formGroup = new FormGroup({
       clearAssignments: new FormControl(false),
     });
+  }
+
+  searchResources(e) {
+    const {
+      target: { value },
+    } = e;
+
+    this.resourcesFiltered = [...this.resources];
+
+    if (value === '') {
+      return;
+    } else {
+      this.resourcesFiltered = filter(this.resourcesFiltered, resource => lowerCase(resource.name).includes(lowerCase(value)));
+    }
   }
 
   closeResources() {
